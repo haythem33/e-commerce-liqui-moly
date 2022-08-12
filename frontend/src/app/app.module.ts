bf49bf5b9a6245fe27a5c87ee4c32db978f05e83
haythem33 <haythembenjbara@gmail.com>
2022-08-12 16:03:27 +0100

    [TEMPLATE] NAVBAR INTEGRATION

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AuthModule } from './auth/auth.module';
 import { StoreModule } from '@ngrx/store';
+import { AuthModule } from '@angular/fire/auth';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +16,10 @@ import { environment } from '../environments/environment';
     BrowserAnimationsModule,
     AuthModule,
     StoreModule.forRoot({}, {}),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   bootstrap: [AppComponent],
 })
---
+++ frontend/src/app/client/client.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientRoutingModule } from './client-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+@NgModule({
+  declarations: [HomeComponent, AboutUsComponent, ContactUsComponent],
+  imports: [CommonModule, ClientRoutingModule],
+})
+export class ClientModule {}

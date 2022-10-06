d62cbf931dbe40dfd6f2844a5cbccbc85ecb7e5e
haythem33 <haythembenjbara@gmail.com>
2022-10-06 14:03:33 +0100

    feature(HOME PAGE TEMPLATE)

--- frontend/src/app/client/client.module.ts
+++ frontend/src/app/client/client.module.ts
@@ -6,9 +6,10 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { CoreModule } from '../core/core.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HomeComponent, AboutUsComponent, ContactUsComponent],
+  declarations: [HomeComponent, AboutUsComponent, ContactUsComponent, FooterComponent],
   imports: [CommonModule, ClientRoutingModule, CoreModule],
 })
 export class ClientModule {}
